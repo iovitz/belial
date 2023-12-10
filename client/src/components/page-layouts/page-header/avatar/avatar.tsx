11d1e53b9280913db1b6701abc2c37c231a1a44b
iovitz <iovitz1999@gmail.com>
2023-12-10 10:25:10 +0800

    chroe: update avatar.tsx

--- client/src/components/page-layouts/page-header/avatar/avatar.tsx
+++ client/src/components/page-layouts/page-header/avatar/avatar.tsx
@@ -5,6 +5,10 @@ import { UserOutlined } from '@ant-design/icons'
 
 export default function UserAvatar() {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false)
+  // const title = {
+  //   login: 'Login To Your Account',
+  //   register: 'Create Your Account',
+  // }
   function handleAvatarClick() {
     setIsLoginModalOpen(true)
   }
@@ -12,9 +16,10 @@ export default function UserAvatar() {
   function handleCloseLoginModal() {
     setIsLoginModalOpen(false)
   }
+
   return (
     <>
-      <Modal title='Login / Register' open={isLoginModalOpen} footer={null} onCancel={handleCloseLoginModal}>
+      <Modal title='Login To Your Account' open={isLoginModalOpen} footer={null} onCancel={handleCloseLoginModal}>
         /awef
       </Modal>
       <Avatar className={styles['header-avatar']} icon={<UserOutlined />} onClick={handleAvatarClick} />

14c2e76a09d97f76fd98ccc0d0aa4fd3424dbbf2
iovitz <iovitz1999@gmail.com>
2024-02-04 20:40:42 +0800

    fix: 修复引入出错的问题

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import RouterElements from './pages/routes'
 import ErrorHandler from './components/error-handler/error-handler'
-import { useGlobalI18n } from './hooks/useGlobalI18n'
+import { useGlobalI18n } from './hooks/i18n'
 
 const App: React.FC = () => {
   useGlobalI18n()

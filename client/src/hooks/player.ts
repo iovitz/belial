0dae8b325677ea373b06282b3fc5153ee4a3bbf4
iovitz <iovitz1999@gmail.com>
2024-03-14 00:18:02 +0800

    feat: 添加视频观看页代码

---
+++ client/src/hooks/player.ts
@@ -0,0 +1,22 @@
+import { RefObject, useEffect } from 'react'
+import Plyr from 'plyr'
+import 'plyr/dist/plyr.css'
+import { logger } from '@/common/logger'
+
+interface HookOptions {
+  divRev: RefObject<HTMLVideoElement>
+  plyrOptions: Plyr.Options
+  onReady: (event: Plyr.PlyrEvent) => void
+}
+
+export function usePlayer({ divRev, plyrOptions, onReady }: HookOptions) {
+  logger.verbose('加载播放器')
+  useEffect(() => {
+    const player = new Plyr(divRev.current!, plyrOptions)
+    player.on('ready', (e) => {
+      onReady(e)
+      logger.verbose('播放器加载成功', e)
+    })
+    return () => {}
+  }, [])
+}

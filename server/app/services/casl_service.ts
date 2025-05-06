a530d467abafc02f6f490352e8682f22964d7374
iovitz <iovitz1999@gmail.com>
2025-05-06 21:05:33 +0800

    feat: 添加casl策略权限服务

---
+++ server/app/services/casl_service.ts
@@ -0,0 +1,29 @@
+import { AbilityBuilder, buildMongoQueryMatcher, createMongoAbility } from '@casl/ability'
+import { allInterpreters, allParsingInstructions } from '@ucast/mongo2js'
+
+const conditionsMatcher = buildMongoQueryMatcher(allParsingInstructions, allInterpreters)
+
+export type ActionStr = 'read' | 'update' | 'create' | 'delete'
+
+interface Ability {
+  action: ActionStr | ActionStr[]
+  cannot?: boolean
+  fields?: string[]
+  conditions?: Record<string, any>
+}
+
+export class CaslService {
+  buildAbilitiesFor(subject: string, _abilities: Array<Ability | null | undefined>) {
+    const { can, cannot, build } = new AbilityBuilder(createMongoAbility)
+    _abilities.forEach((ability) => {
+      if (!ability) {
+        return
+      }
+
+      const { action, cannot: cannotDoThis, fields, conditions } = ability
+      return (cannotDoThis ? cannot : can)(action, subject, fields, conditions)
+    })
+
+    return build({ conditionsMatcher, detectSubjectType: () => subject })
+  }
+}
